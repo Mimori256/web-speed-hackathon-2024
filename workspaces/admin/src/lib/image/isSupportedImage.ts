9477dc3136287390d3d7fca2af898b976d208e66
Mimori256 <mimorimori256@gmail.com>
2024-03-24 11:30:22 +0900

    Remove `FileType` package in server

--- workspaces/admin/src/lib/image/isSupportedImage.ts
+++ workspaces/admin/src/lib/image/isSupportedImage.ts
@@ -1,10 +1,8 @@
-import { fileTypeFromBuffer } from 'file-type';
-
-const SUPPORTED_MIME_TYPE_LIST = ['image/bmp', 'image/jpeg', 'image/png', 'image/webp', 'image/avif', 'image/jxl'];
+const SUPPORTED_EXTENSION_LIST = ['bmp', 'jpeg', 'jpg', 'png', 'webp', 'avif', 'jxl'];
 
 export async function isSupportedImage(image: File): Promise<boolean> {
-  const fileType = await fileTypeFromBuffer(new Uint8Array(await image.arrayBuffer()));
-  if (SUPPORTED_MIME_TYPE_LIST.includes(fileType?.mime ?? '')) {
+  const extension = image.name.split('.').pop()?.toLowerCase();
+  if (extension && SUPPORTED_EXTENSION_LIST.includes(extension)) {
     return true;
   }
 
